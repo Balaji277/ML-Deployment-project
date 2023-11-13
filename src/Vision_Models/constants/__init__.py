645b1268138430f585f62f9b70cd606dfbefc418
Balaji Praneeth Boga <baluboga2707@gmail.com>
2023-11-13 11:59:39 -0500

    added main, pipeline with data ingestion

---
+++ src/Vision_Models/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path(r"C:\Users\balub\OneDrive\Desktop\Balaji sample scripts\ML-Deployment-project\config\config.yaml")
+PARAMS_FILE_PATH = Path(r"C:\Users\balub\OneDrive\Desktop\Balaji sample scripts\ML-Deployment-project\params.yaml")
