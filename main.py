b8f0bd41b068c132ad614715c0df634f22277f43
Balaji Praneeth Boga <baluboga2707@gmail.com>
2023-11-13 00:41:33 -0500

    latest 13th Nov 12:41 am

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.Vision_Models import logger
+
+logger.info("Hello World! Welcome to ML Project")
+
+
--- template.py
+++ template.py
@@ -11,6 +11,7 @@ list_of_files = [
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
@@ -21,7 +22,8 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
+    "templates/index.html",
+    "main.py"
 
 ]
 
