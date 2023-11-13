a61e25791de5b6c7d40e555846a2d0c8675dc6af
Balaji Praneeth Boga <baluboga2707@gmail.com>
2023-11-13 13:10:38 -0500

    evaluation added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Vision_Models import logger
 from Vision_Models.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Vision_Models.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from Vision_Models.pipeline.stage_03_training import ModelTrainingPipeline
-# from Vision_Models.pipeline.stage_04_evaluation import EvaluationPipeline
+from Vision_Models.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -48,17 +48,17 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Evaluation stage"
-# try:
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_evalution = EvaluationPipeline()
-#    model_evalution.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
